520b235b4507c4018be9201d0b73a28f987baf36
Sutthirath Sean Vilaysane <seanvilaysane@gmail.com>
2018-11-05 10:49:55 -0800

    Added mapbox bus stop rendering

--- static/js/map.js
+++ static/js/map.js
@@ -24,60 +24,6 @@ const map = (() => {
     // Map display controls
     map.addControl(new mapboxgl.NavigationControl());
 
-    // // START TEST
-    // var title = document.getElementById('location-title');
-    // var description = document.getElementById('location-description');
-
-    // var locations = [{
-    //     "id": "2",
-    //     "title": "The Bronx",
-    //     "description": "This is where hip-hop was born, where the Yankees became a dynasty and where you can find New York City's leading zoo and botanical garden.",
-    //     "camera": {
-    //         center: [-73.8709, 40.8255],
-    //         zoom: 12.21,
-    //         pitch: 50
-    //     }
-    // }, {
-    //     "id": "3",
-    //     "title": "Brooklyn",
-    //     "description": "No matter how hip it looks on TV, NYC's most populous borough is best experienced in person. Read on to find out about live music, Prospect Park, Nets basketball and more.",
-    //     "camera": {
-    //         center: [-73.9499, 40.6260],
-    //         bearing: -8.9,
-    //         zoom: 11.68
-    //     }
-    // }];
-
-    // function highlightBorough(code) {
-    //     // Only show the polygon feature that cooresponds to `borocode` in the data
-    //     map.setFilter('highlight', ["==", "borocode", code]);
-    // }
-    
-    // function playback(index) {
-    //     title.textContent = locations[index].title;
-    //     description.textContent = locations[index].description;
-    
-    //     highlightBorough(locations[index].id ? locations[index].id : '');
-    
-    //     // Animate the map position based on camera properties
-    //     map.flyTo(locations[index].camera);
-    
-    //     map.once('moveend', function() {
-    //         // Duration the slide is on screen after interaction
-    //         window.setTimeout(function() {
-    //             // Increment index
-    //             index = (index + 1 === locations.length) ? 0 : index + 1;
-    //             playback(index);
-    //         }, 3000); // After callback, show the location for 3 seconds.
-    //     });
-    // }
-    
-    // // Display the last title/description first
-    // title.textContent = locations[locations.length - 1].title;
-    // description.textContent = locations[locations.length - 1].description;
-
-    // // END TEST
-
     // Render bus stops on map
     map.on('click', function () {
         let points = 0;
@@ -106,25 +52,6 @@ const map = (() => {
                 }
             });
         });
-
-        // map.addLayer({
-        //     "id": "highlight",
-        //     "type": "fill",
-        //     "source": {
-        //         "type": "vector",
-        //         "url": "mapbox://mapbox.8ibmsn6u"
-        //     },
-        //     "source-layer": "original",
-        //     "paint": {
-        //         "fill-color": "#fd6b50",
-        //         "fill-opacity": 0.25
-        //     },
-        //     "filter": ["==", "borocode", ""]
-        // }, 'neighborhood_small_label'); // Place polygon under the neighborhood labels.
-    
-        // // Start the playback animation for each borough
-        // playback(0);
-
     });
 
     // Current location
